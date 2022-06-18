dad01b0292738ae948f61677c81de30be23df383
LaZeAsh <lazeashyt@gmail.com>
2022-06-18 07:55:22 -0400

    Initial Commit

---
+++ src/database/mongo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+export function connect() {
+    const options = {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useCreateIndex:true,
+        useFindAndModify: false,
+    };
+    mongoose.connect(process.env.MONGO_DB_PASSWORD, options).then(() => console.log(`MongoDB is connected`))
+}
